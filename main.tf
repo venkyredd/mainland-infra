9e995b5574621d28e802053066b133acb937c4f6
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-04-23 00:24:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "mainland-static-site-bucket"
+  bucket = "v-mainland-static-site-bucket"
   force_destroy = true
 
   tags = {
